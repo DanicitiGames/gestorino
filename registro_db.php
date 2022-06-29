2215700c7399c73354556b5c958a60e8cd7726b8
Daniciti <61484033+DanicitiGames@users.noreply.github.com>
2022-06-29 19:59:40 -0300

    Update registro_db.php

--- registro_db.php
+++ registro_db.php
@@ -4,8 +4,12 @@ $numbers = str_split("1234567890");
 $simbols = str_split("-_=!@#&.,;");
 $username_split = str_split($_POST['username']);
 $username_text = "";
-$has_space = false;
-$invalid_character = false;
+$username_has_space = false;
+$username_invalid_character = false;
+$password_split = str_split($_POST['password']);
+$password_text = "";
+$password_has_space = false;
+$password_invalid_character = false;
 
 for($a = 0; $a < count($username_split); $a++){
     $finished = false;
@@ -13,7 +17,7 @@ for($a = 0; $a < count($username_split); $a++){
     while(!$finished){
         if($username_split[$a] == " "){
             $username_text .= " ";
-            $has_space = true;
+            $username_has_space = true;
             $finished = true;
         }else{
             $not_found++;
@@ -48,27 +52,39 @@ for($a = 0; $a < count($username_split); $a++){
             }
         }
         if($not_found == 4){
-            $invalid_character = true;
+            $username_invalid_character = true;
             $finished = true;
         }
     }
 }
+function redirect($error) {
+    ob_start();
+    header('Location: '."https://localhost/registro.php?error=$error");
+    ob_end_flush();
+    die();
+}
 if(strlen($username_text) == 0){
     if(strlen($_POST['username']) == 0){
         echo "erro: Escreva algo<br>";
+        redirect(1);
     }else{
         echo "erro: Usuário inválido<br>";	
+        redirect(2);
     }
 }
 elseif(strlen($username_text) <= 3 || strlen($username_text) >= 25){
     $len = strlen($username_text);
+    redirect(3);
     echo "erro: O usuário tem que ter no mínimo 4 e no máximo 24 caracteres<br> $len";
 }
-elseif($has_space){
+elseif($username_has_space){
     $with_point = str_replace(" ",".",$username_text);
     echo "erro: O usuário não pode conter espaços<br>Mas você pode tentar usar o nome: $with_point<br>";
-}elseif($invalid_character){
+    redirect(4);
+}elseif($username_invalid_character){
     echo "erro: O usuário não pode conter caracteres especiais não permitidos<br>Permitidos: _-=!@#&.,;<br>";
+    redirect(5);
+}else{
+
 }
-echo "texto = $username_text <br> texto_str=$username_text<br>";
 ?>

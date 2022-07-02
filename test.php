2c4a33951b024b31d58b8770348a958c06a2e1b3
Daniciti <61484033+DanicitiGames@users.noreply.github.com>
2022-07-01 22:10:12 -0300

    Delete test.php

--- test.php
+++
@@ -1,7 +0,0 @@
-<form action="registro_db.php" method="POST">
-    <div class="mb-1 mt-5">
-        <label for="text" class="form-label">Usuário:</label>
-    <input type="text" class="form-control" placeholder="Seu usuário" name="username">
-    </div>
-    <button type="submit" class="btn btn-primary">testar</button>
-</form>

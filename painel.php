892e2bc715867126f228b0f0df85339b08eaebb8
Daniciti <61484033+DanicitiGames@users.noreply.github.com>
2022-07-24 07:55:48 -0300

    Update painel.php

--- painel.php
+++ painel.php
@@ -1,119 +1,114 @@
 <?php
-if(!isset($_SESSION)){ 
+if (!isset($_SESSION)) {
   session_start();
 }
-include('deslogado.php');
+include('logado.php');
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="pt">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Gestorino</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Gestorino</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.js"></script>
 </head>
-<body style="background-color: #363636;max-width: 100%;overflow-x: hidden;">
+
+<body style="max-width: 100%; overflow-x: hidden;">
   <div style="background-image: linear-gradient(to bottom right, #ce93d8, #81d4fa);">
-  <div class="row shadow mb-5">
-<nav class="navbar navbar-expand-sm navbar-light">
-  <div class="container-fluid">
-  <ul class="navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link active" href="index.php"><h3 style="color:#574764">Gestorino</h3></a>
-        </li>
-    </ul>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="nav">
-        <ul class="navbar-nav me-auto">
+    <div class="row shadow">
+      <nav class="navbar navbar-expand-sm navbar-light">
+        <div class="container-fluid">
+          <ul class="navbar-nav">
             <li class="nav-item">
+              <a class="nav-link active" href="index.php">
+                <h3 style="color:#574764"><?php echo $_SESSION['username']; ?></h3>
+              </a>
+            </li>
+          </ul>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="nav">
+            <ul class="navbar-nav me-auto">
+              <li class="nav-item">
                 <a class="nav-link" href="faq.php">FAQ</a>
-            </li>
-            <li class="nav-item">
+              </li>
+              <li class="nav-item">
                 <a class="nav-link" href="sobre.php">Sobre</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="contatos.php">Contato</a>
-            </li>
-        </ul>
-        <div class="d-flex">
-        <ul class="navbar-nav me-auto">
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contato.php">Contato</a>
+              </li>
+            </ul>
+            <div class="d-flex">
+              <ul class="navbar-nav me-auto">
+                <li class="nav-item"><a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#myModal">Perfil</a></li>
+                <li class="nav-item"><a class="nav-link" href="javascript:void(0)"><strong>Painel</strong></a></li>
+                <li class="nav-item"><a class="nav-link" href="deslogar.php">Sair</a></li>'
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
     <?php
-    if(count($_SESSION) == 0){
-        echo '<li class="nav-item"><a class="nav-link" href="registro.php">Registro</a></li>
-              <li class="nav-item"><a class="nav-link" href="javascript:void(0)"><strong>Login</strong></a></li>';
-    }else{
-        echo '<li class="nav-item"><a class="nav-link" href="painel.php">Painel</a></li>
-              <li class="nav-item"><a class="nav-link" href="deslogar.php">Sair</a></li>';
+    if(!isset($_POST['switch'])){
+      echo '<form method="post">
+      <nav class="navbar navbar-expand-sm navbar-light">
+      <input type="submit" class="btn shadow" style="color:#574764" name="switch" id="Cadastro" value="Cadastro"/>
+      <input type="submit" class="btn" style="color:#574764" name="switch" id="Principal" value="Principal"/>
+      <input type="submit" class="btn" style="color:#574764" name="switch" id="Gráficos" value="Gráficos"/>
+      <input type="submit" class="btn" style="color:#574764" name="switch" id="Histórico" value="Histórico"/>
+      </nav>
+      </form>
+      <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>';
+    }
+    function funcao()
+    {
+      if($_POST['switch'] == 'Gráficos'){
+        include('graficos.php');
+      }
+      if($_POST['switch'] == 'Cadastro'){
+        echo '<form method="post">
+        <nav class="navbar navbar-expand-sm navbar-light">
+        <input type="submit" class="btn shadow" style="color:#574764" name="switch" id="Cadastro" value="Cadastro"/>
+        <input type="submit" class="btn" style="color:#574764" name="switch" id="Principal" value="Principal"/>
+        <input type="submit" class="btn" style="color:#574764" name="switch" id="Gráficos" value="Gráficos"/>
+        <input type="submit" class="btn" style="color:#574764" name="switch" id="Histórico" value="Histórico"/>
+        </nav>
+        </form>
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>';
+      }
+      if($_POST['switch'] == 'Principal'){
+        echo '<form method="post">
+        <nav class="navbar navbar-expand-sm navbar-light">
+        <input type="submit" class="btn" style="color:#574764" name="switch" id="Cadastro" value="Cadastro"/>
+        <input type="submit" class="btn shadow" style="color:#574764" name="switch" id="Principal" value="Principal"/>
+        <input type="submit" class="btn" style="color:#574764" name="switch" id="Gráficos" value="Gráficos"/>
+        <input type="submit" class="btn" style="color:#574764" name="switch" id="Histórico" value="Histórico"/>
+        </nav>
+        </form>
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>';
+      }
+      if($_POST['switch'] == 'Histórico'){
+        echo '<form method="post">
+        <nav class="navbar navbar-expand-sm navbar-light">
+        <input type="submit" class="btn" style="color:#574764" name="switch" id="Cadastro" value="Cadastro"/>
+        <input type="submit" class="btn" style="color:#574764" name="switch" id="Principal" value="Principal"/>
+        <input type="submit" class="btn" style="color:#574764" name="switch" id="Gráficos" value="Gráficos"/>
+        <input type="submit" class="btn shadow" style="color:#574764" name="switch" id="Histórico" value="Histórico"/>
+        </nav>
+        </form>
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>';
+      }
+    }
+    if (array_key_exists('switch', $_POST)) {
+      funcao($_POST);
     }
     ?>
-        </ul>
-        </div>
-    </div>
-  </div>
-</nav> 
-</div>
-        <div class="row" style="color: #ECF0F1">
-            <div class="col"></div>
-            <div class="col"></div>
-            <div class="col">
-            <form action="login_db.php" method="POST">
-                <div class="mb-1 mt-5">
-<?php
-$x = parse_url($_SERVER['REQUEST_URI']);
-if(count($x) >= 2){
-  if($x['query']){
-    $err = "error=";
-    $len = strlen($err);
-    if(substr($x['query'], 0, $len) === $err){
-      $error = substr($x['query'], $len, 2);
-      $error_message = "";
-      $valid = true;
-      switch($error){
-        case "01":
-          $error_message = "Usuário inválido";
-          break;
-        case "02":
-          $error_message = "Escreva o usuário";
-          break;
-        case "03":
-          $error_message = "Senha inválida";
-          break;
-        case "04":
-          $user = substr($x['query'], 9);
-          $error_message = "Escreva uma senha";
-          break;
-        case "05":
-          $error_message = "O usuário ou senha estão incorretos!";
-          break;
-        default:
-          $valid = false;
-          break;
-    }
-    if($valid){
-      echo "<div class='alert alert-danger mb-1 mt-5'>$error_message</div>";  
-    }
-  }
-}
-}
-?>
-                    <label for="text" class="form-label">Usuário:</label>
-                    <input type="text" class="form-control" placeholder="Seu usuário" name="username">
-                </div>
-                <div class="mb-3">
-                    <label for="pwd" class="form-label">Senha:</label>
-                    <input type="password" class="form-control" placeholder="Sua senha" name="password">
-                </div>
-                <button type="submit" class="btn btn-primary">Logar</button>
-            </form>
-        </div>
-        <div class="col"></div>
-        <div class="col"></div>
-        </div>
-    </div>
-</div>
 </body>
 </html>
